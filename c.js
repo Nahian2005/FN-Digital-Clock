31ad65c267b49d5affc9d15b7144ea4c7c3d15da
Nahian2005 <118044869+Nahian2005@users.noreply.github.com>
2024-08-24 01:49:47 +0600

    Add files via upload

---
+++ c.js
@@ -0,0 +1,18 @@
+
+
+
+setInterval(t, 1000);
+function t(){
+    const c= document.querySelector("div #h")
+
+    const date=new Date();
+    
+c.innerHTML=date.toLocaleTimeString();
+  
+}
+
+
+
+
+
+
